e54a5fc5f8dcfe593e704192a84f0a593dc30637
Ely M. Spears <spearsem@gmail.com>
2014-03-18 16:35:44 -0400

    Initial add for IPython, emacs, and bash.

---
+++ emacs_configs/.emacs
@@ -0,0 +1,252 @@
+;; .emacs
+
+;; Load package repositories
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/package/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Add jedi to python-mode.
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; Set color theme
+(load-theme 'tango-dark)
+
+;; Set font
+(set-frame-font "Courier 10 Pitch-12" nil t)
+(set-default-font "Courier 10 Pitch-12")
+(defun fontify-frame (frame)
+  (set-frame-parameter frame 'font "Courier 10 Pitch-12"))
+;; Fontify current frame
+(fontify-frame nil)
+;; Fontify any future frames
+(push 'fontify-frame after-make-frame-functions)
+
+;; Require load for column mode and set active
+;;(require 'column-enforce-mode)
+;;(add-hook 'python-mode-hook 'column-enforce-mode)
+
+;;;;;;;;;;;
+;; Modes ;;
+;;;;;;;;;;;
+
+;; Turn on font-lock mode
+(when (fboundp 'global-font-lock-mode)
+      (global-font-lock-mode t))
+
+;; Turn on overwrite/selection delete mode.
+(delete-selection-mode t)
+
+;; Enable visual feedback on selections
+;; (setq transient-mark-mode t))
+
+;; Uncomment to disable loading of 'default.el' at startup.
+;; (setq inhibit-default-init t)
+
+;; Improved frame titles
+(setq frame-title-format
+      (concat "%b - emacs@" (system-name)))
+
+;; Default to unified diffs
+(setq diff-switches "-u")
+
+;; Always end with a newline
+;; (setq require-final-newline 'query)
+
+;; Allow Ctl-n n to narrow on region.
+(put 'narrow-to-region 'disabled nil)
+
+;; Toggle horizontal or vertical split in 2-window layout.
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+         (next-win-buffer (window-buffer (next-window)))
+         (this-win-edges (window-edges (selected-window)))
+         (next-win-edges (window-edges (next-window)))
+         (this-win-2nd (not (and (<= (car this-win-edges)
+                     (car next-win-edges))
+                     (<= (cadr this-win-edges)
+                     (cadr next-win-edges)))))
+         (splitter
+          (if (= (car this-win-edges)
+             (car (window-edges (next-window))))
+          'split-window-horizontally
+        'split-window-vertically)))
+    (delete-other-windows)
+    (let ((first-win (selected-window)))
+      (funcall splitter)
+      (if this-win-2nd (other-window 1))
+      (set-window-buffer (selected-window) this-win-buffer)
+      (set-window-buffer (next-window) next-win-buffer)
+      (select-window first-win)
+      (if this-win-2nd (other-window 1))))))
+
+;; Bind above function to "Ctrl-x 4 t" to toggle.
+(define-key ctl-x-4-map "t" 'toggle-window-split)
+
+;; Swap buffers into different windows
+(defun swap-buffer ()
+  (interactive)
+  (cond ((one-window-p) (display-buffer (other-buffer)))
+        ((let* ((buffer-a (current-buffer))
+                (window-b (cadr (window-list)))
+                (buffer-b (window-buffer window-b)))
+           (set-window-buffer window-b buffer-a)
+           (switch-to-buffer buffer-b)))))
+           ;; Use following to always swap into the primary window.
+           ;;(other-window 1)))))
+
+;; Bind the above function to Ctrl-x c
+(define-key ctl-x-map "c" 'swap-buffer)
+
+;; Set buffer console to always show current system name
+;; and the whole file path of the buffer being edited.
+(setq frame-title-format
+      (list (format "%s %%S: %%j " (system-name))
+        '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
+
+;; Make a non-killing version of the rectangle copy, and
+;; bind it to C-x r M-w
+(defun my-copy-rectangle (start end)
+   "Copy the region-rectangle instead of `kill-rectangle'."
+   (interactive "r")
+   (setq killed-rectangle (extract-rectangle start end))) 
+(global-set-key (kbd "C-x r M-w") 'my-copy-rectangle)
+
+;; Functions to list currently-open buffers in order by their size.
+(defmacro with-buffer-set (b &rest args)
+  (list 'save-excursion
+        (list 'set-buffer b)
+        (cons 'progn args)))
+
+;; From Mark Weissman.
+(defun list-sized-buffers ()
+  (interactive)
+  (let ((standard-output standard-output)
+    (files-only nil)
+    (old-buffer (current-buffer)))
+    (with-buffer-set
+     (get-buffer-create "*Buffers*")
+     (setq buffer-read-only nil)
+     (erase-buffer)
+     (beginning-of-buffer)
+     (insert " MR Buffer           Size  Mode         File\n")
+     (insert " -- ------           ----  ----         ----\n")
+     (setq standard-output (current-buffer))
+     (dolist (buffer (mapcar 'cdr (sort
+                   (mapcar '(lambda (b) 
+                               (with-buffer-set b 
+                                 (cons (point-max) (current-buffer))))
+                       (buffer-list))
+                   '(lambda (x y) (> (car x) (car y))))))
+       (let ((name (buffer-name buffer))
+         (file (buffer-file-name buffer))
+         this-buffer-line-start
+         this-buffer-read-only
+         (this-buffer-size (buffer-size buffer))
+         this-buffer-mode-name
+         this-buffer-directory)
+     (with-current-buffer buffer
+       (setq this-buffer-read-only buffer-read-only
+         this-buffer-mode-name mode-name)
+       (unless file
+         ;; No visited file.  Check local value of
+         ;; list-buffers-directory.
+         (when (and (boundp 'list-buffers-directory)
+            list-buffers-directory)
+           (setq this-buffer-directory list-buffers-directory))))
+     (cond
+      ;; Don't mention internal buffers.
+      ((string= (substring name 0 1) " "))
+      ;; Maybe don't mention buffers without files.
+      ((and files-only (not file)))
+      ((string= name "*Buffer List*"))
+      ;; Otherwise output info.
+      (t
+       (setq this-buffer-line-start (point))
+       ;; Identify current buffer.
+       (if (eq buffer old-buffer)
+           (progn
+         (setq desired-point (point))
+         (princ "."))
+         (princ " "))
+       ;; Identify modified buffers.
+       (princ (if (buffer-modified-p buffer) "*" " "))
+       ;; Handle readonly status.  The output buffer is special
+       ;; cased to appear readonly; it is actually made so at a
+       ;; later date.
+       (princ (if (or (eq buffer standard-output)
+              this-buffer-read-only)
+              "% "
+            "  "))
+       (princ name)
+       ;; Put the buffer name into a text property
+       ;; so we don't have to extract it from the text.
+       ;; This way we avoid problems with unusual buffer names.
+       (setq this-buffer-line-start
+         (+ this-buffer-line-start Buffer-menu-buffer-column))
+       (let ((name-end (point)))
+         (indent-to 17 2)
+         (put-text-property this-buffer-line-start name-end
+                'buffer-name name)
+         (put-text-property this-buffer-line-start (point)
+                'buffer buffer)
+         (put-text-property this-buffer-line-start name-end
+                'mouse-face 'highlight)
+         (put-text-property this-buffer-line-start name-end
+                'help-echo "mouse-2: select this buffer"))
+       (let ((size (format "%8d" this-buffer-size))
+         (mode this-buffer-mode-name)
+         (excess (- (current-column) 17)))
+         (while (and (> excess 0) (= (aref size 0) ?\ ))
+           (setq size (substring size 1)
+             excess (1- excess)))
+         (princ size)
+         (indent-to 27 1)
+         (princ mode))
+       (indent-to 40 1)
+       (or file (setq file this-buffer-directory))
+       (when file
+         (princ (abbreviate-file-name file)))
+       (princ "\n")))))
+     (Buffer-menu-mode)
+     (beginning-of-buffer)
+     (display-buffer (current-buffer)))))
+;; end of list-sized-buffers
+
+;; Word and character count in highlighted region.
+(defun count-words-region (posBegin posEnd)
+  "Print number of words and chars in region."
+  (interactive "r")
+  (message "Counting: ")
+  (save-excursion
+    (let (wordCount charCount)
+        (setq wordCount 0)
+        (setq charCount (- posEnd posBegin))
+        (goto-char posBegin)
+        (while (and (< (point) posEnd)
+                    (re-search-forward "\\w+\\W*" posEnd t))
+          (setq wordCount (1 + wordCount)))
+
+        (message "Words: %d. Chars: %d." wordCount charCount))))
+
+;; Bind word count function to C-x M-c
+(global-set-key (kbd "C-x M-c") 'count-words-region)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modes handled by Custom. ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Variables set by saving options for menus/parentheses, etc.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
---
+++ ipython_configs/profile_default/launch.py
@@ -0,0 +1,373 @@
+"""
+IPython launch script
+Author: Ely M. Spears
+"""
+
+import re
+import os
+import abc
+import sys
+import mock
+import time
+import types
+import pandas
+import inspect
+import cPickle
+import unittest
+import operator
+import warnings
+import datetime
+import dateutil
+import calendar
+import copy_reg
+import itertools
+import contextlib
+import collections
+import numpy as np
+import scipy as sp
+import scipy.stats as st
+import scipy.weave as weave
+import multiprocessing as mp
+from dateutil.relativedelta import relativedelta as drr
+from IPython.core.magic import (Magics,
+                                register_line_magic,
+                                register_cell_magic,
+                                register_line_cell_magic)
+
+###########################
+# Pickle/Unpickle methods #
+###########################
+
+# See explanation at:
+# < http://bytes.com/topic/python/answers/
+#   552476-why-cant-you-pickle-instancemethods >
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+
+#############
+# Utilities #
+#############
+def interface_methods(*methods):
+    """
+    Class decorator that can decorate an abstract base class with method names
+    that must be checked in order for isinstance or issubclass to return True.
+    """
+    def decorator(Base):
+        def __subclasshook__(Class, Subclass):
+            if Class is Base:
+                all_ancestor_attrs = [ancestor_class.__dict__.keys()
+                                      for ancestor_class in Subclass.__mro__]
+                if all(method in all_ancestor_attrs for method in methods):
+                    return True
+            return NotImplemented
+        Base.__subclasshook__ = classmethod(__subclasshook__)
+        return Base
+
+def interface(*attributes):
+    """
+    Class decorator checking for any kind of attributes, not just methods.
+
+    Usage:
+
+    @interface(('foo', 'bar', 'baz))
+    class Blah
+       pass
+
+    Now, new classes will be treated as if they are subclasses of Blah, and 
+    instances will be treated instances of Blah, provided they possess the
+    attributes 'foo', 'bar', and 'baz'.
+    """
+    def decorator(Base):
+        def checker(Other):
+            return all(hasattr(Other, a) for a in attributes)
+        
+        def __subclasshook__(cls, Other):
+            if checker(Other):
+                return True
+            return NotImplemented
+        
+        def __instancecheck__(cls, Other):
+            return checker(Other)
+
+        Base.__metaclass__.__subclasshook__ = classmethod(__subclasshook__)
+        Base.__metaclass__.__instancecheck__ = classmethod(__instancecheck__)
+        return Base
+    return decorator
+
+def ensure_return_type(permissible_types):
+    """
+    Decorator factory for method decoration. Forces output of decorated
+    function to be a type from the tuple `permissible_types`.
+    """
+    from functools import wraps
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            potential_return = func(*args, **kwargs)
+            if isinstance(potential_return, permissible_types):
+                return potential_return
+                #Note: could support a plain callable argument, instead of 
+                #the tuple of types. Then replace the if-condition above
+                #with calling that callable on the potential return value.
+            else:
+                err_msg = "Return values `{}` must be a type from {}".format(
+                    potential_return, permissible_types)
+                raise TypeError(err_msg)
+        return wrapper
+    return decorator
+
+def fn_unique(lst, cmp_op=operator.ne):
+    """
+    Creates an order-preserving new list from an input list, such that only
+    the unique elements from the original list appear. Optionally can be used
+    with a different comparison operator. Inspired by Haskell's `nub` function.
+    """
+    f = lambda x: cmp_op(x, lst[0])
+    return [] if lst == [] else lst[0:1] + fn_unique(filter(f, lst[1:]), cmp_op)
+
+def fn_get_nth(seq, cond_callable, n=1):
+    """
+    Among all entries x within `seq` such that cond_callable(x) evaluates to
+    True, return the n-th. Returns None if StopIteration or other issues cause
+    an error in finding the n-th occurrence.
+    """
+    islice, ifilter = itertools.islice, itertools.ifilter
+    try:
+        return (islice(ifilter(cond_callable, seq), n-1, n).next() if n >= 0
+                else fn_get_nth(reversed(seq), cond_callable, -n))
+    except:
+        return None
+    
+def fn_context(call):
+    """
+    Create an on-the-fly context manager out of an arbitrary callable. E.g.:
+
+        `with fn_context(set)(my_list) as s:`
+
+    will create a `set` out of `my_list` by calling `set(my_list)` and then
+    feeding it as the context manager for a `with` statement. Works with any
+    callable in place of `set` in the example.
+    """
+    from contextlib import contextmanager
+    apply_call_generator = lambda x: (call(y) for y in (x,))
+    return contextmanager(apply_call_generator)
+
+def fn_debug(f, *args, **kwargs):
+    """
+    Feed a function and args into the IPython debugger. Some other useful
+    debugging options: 
+
+        python -m pdb file.py
+        %run -d file.py
+    """
+    from IPython.core.debugger import Pdb
+    return Pdb(color_scheme="Linux").runcall(f, *args, **kwargs)
+
+def fn_set_trace():
+    """
+    Sets a more feature-rich version of set_trace.
+    """
+    from IPython.core.debugger import Pdb
+    Pdb(color_scheme="Linux").set_trace(sys._getframe().f_back)
+
+def fn_console():
+    """
+    Prompt a code console to explore local variables. Similar to
+    setting a trace.
+    """
+    import code
+    code.interact(local=locals())
+
+class attrdict(dict):
+    """
+    Inherits from dict and sets __getattr__ to mirror __getitem__, allowing
+    dot-based access to the dict's keys.
+    """
+    def __getattr__(self, attr):
+        return self[attr]
+
+class SliceMaker(object):
+    """
+    __getitem__ for this class just returns the "item" (slice) that was
+    requested, so it is a factory for slices. So if x = SliceMaker(), then
+    asking for x[some_slice] just returns some_slice as a slice object.
+
+    This allows you to compactly pass around the slice syntax. Similar to
+    the numpy s_ function.
+    """
+    def __getitem__(self, item):
+        return item
+
+class SelectableDataFrame(pandas.DataFrame):
+    """
+    Subclasses DataFrame purely to mokeypatch a select function. 
+    """
+    def select(self, column, value=None, op=None):
+        """
+        Inputs
+        ------
+        column: A string naming one of the DataFrame's columns.
+        value: A value for comparison. Default is None.
+        op: A callable that accepts either 1 or 2 arguments. For 1
+            argument, op(self[column]) is called. For two,
+            op(self[column], value) is callued. Default is None
+
+        If op and value are None, then it functions to select where
+        the column is not null. If op is None but value is not None,
+        then it is assumed that equality-checking is the requested
+        operation, and will return where self[column] == value. If only
+        value is None, then the op is applied as a unary operator to
+        the column.
+        """
+        if value is None and op is None:
+            condition = self[column].notnull()
+        elif value is not None and op is None:
+            condition = (self[column] == value)
+        elif value is None and op is not None:
+            condition = op(self[column])
+        else:
+            condition = op(self[column], value)
+
+        return SelectableDataFrame(self[condition])
+
+class Infix(object):
+    """
+    Raymond Hettinger infix recipe (from ActiveState code example). Creates
+    an instance object that overrides << and >> to function like an "infix"
+    operator.
+    """
+    def __init__(self, function):
+        self.function = function
+    
+    def __rlshift__(self, other):
+        return Infix(lambda x, self=self, other=other: self.function(other, x))
+    
+    def __rshift__(self, other):
+        return self.function(other)
+
+    def __call__(self, value1, value2):
+        return self.function(value1, value2)
+
+class SelectInfix(Infix):
+    """
+    Special case of Infix that doessome parsing on what is requested
+    and allows for SQL-like statements to appear on the left side.
+    """
+    def __init__(self):
+        self.function = lambda x,y=None: (
+            lambda z: self.parse_select_value(x, z))
+
+    def __mul__(self, other):
+        return self.function("*") << other
+
+    def parse_select_value(self, val, df):
+        if isinstance(val, (list, tuple, set)):
+            return df[list(val)]
+
+        elif isinstance(val, str):
+            if val.startswith("TOP"):
+                str_top, top_val = val.split()
+                return df.head(int(top_val))
+
+            elif val.startswith("BOTTOM"):
+                str_bottom, bottom_val = val.split()
+                return df.tail(int(bottom_val))
+
+            elif val == "*":
+                return df
+                
+class COL(object):
+    """
+    Column object. Wraps a string naming a column in an eventual DataFrame to
+    be used with the infix tools. Creates selection callables based on 
+    supported logic operations.
+    """
+
+
+    def __init__(self, column):
+        self.column = column
+
+    def make_function(self, other, op):
+        return lambda x: SelectableDataFrame(x).select(self.column, other, op)
+
+    def __gt__(self, other):
+        op = operator.gt
+        return self.make_function(other, op)
+
+    def __ge__(self, other):
+        op = operator.ge
+        return self.make_function(other, op)
+
+    def __lt__(self, other):
+        op = operator.lt
+        return self.make_function(other, op)
+
+    def __le__(self, other):
+        op = operator.le
+        return self.make_function(other, op)
+
+    def __eq__(self, other):
+        op, other = ((lambda x: np.isnan(x), None) if np.isnan(other)
+                     else (operator.eq, other))
+        return self.make_function(other, op)
+
+    def __ne__(self, other):
+        op, other = ((lambda x: ~np.isnan(x), None) if np.isnan(other)
+                     else (operator.ne, other))
+        return self.make_function(other, op)
+
+    def __mod__(self, other):
+        op = lambda x, y: x.isin(y)
+        return self.make_function(other, op)
+
+class Timer(object):
+    """
+    Timing context manager. Usage: `with Timer() as foo: ...` then
+    inspect foo.elapsed_time at the end.
+    """
+    def __init__(self, verbose=False, name = None):
+        from timeit import default_timer
+        self.timer, self.verbose = default_timer, verbose
+        self.name = " " if name is None else "'" + name + "'"
+
+    def __enter__(self):
+        self.start = self.timer()
+
+    def __exit__(self, *args):
+        self.stop = self.timer()
+        self.elapsed_time = self.stop - self.start
+        if self.verbose:
+            print ("Block {} elpased time: {}".format(self.name, 
+                                                     self.elapsed_time))
+
+
+
+
+
+###############################
+# Data and instance utilities #
+###############################
+dfrm = pandas.DataFrame(np.random.rand(10,3),
+                        columns=["A", "B", "C"])
+
+tall_dfrm = 'blah' 
+        
+AND = Infix(lambda x, y: y(x))
+FROM = Infix(lambda x, y: x(y))
+WHERE = Infix(lambda x, y: y(x))
+SELECT = SelectInfix()
